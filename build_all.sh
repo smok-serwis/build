daec0acfa73f4c2944b9b16254d92c4097f7e1fc
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-12-02 11:53:29 +0100

    build script added

---
+++ build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+function build_for {
+    cd $1
+    docker build -t "smokserwis/build:$1" .
+    docker push "smokserwis/build:$1"
+    cd ..
+}
+cd dockerfiles
+
+build_for docker-only
+build_for latest
+build_for adk-cordova
+build_for node7
