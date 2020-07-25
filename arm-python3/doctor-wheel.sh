6c23d7f28234494bb581867b6c63ca2441acf01f
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-07-25 16:52:20 +0200

    guess doctor-wheel needed that zip installed

--- arm-python3/doctor-wheel.sh
+++ arm-python3/doctor-wheel.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+WHEEL_NAME=$(basename "$1")
 MY_DIR="$(cat /proc/sys/kernel/random/uuid)"
 mv "$1" "${MY_DIR}"
 cd "${MY_DIR}"
-unzip "$1"
+unzip "$WHEEL_NAME"
 for F in $(find . -iname *.so)
 do
   strip -X -x "$F"
 done
-rm -f "$1"
-zip -r "$1" .
-cp "$1" ..
+rm -f "$WHEEL_NAME"
+zip -r "$WHEEL_NAME" .
+cp "$WHEEL_NAME" ..
 cd ..
 rm -rf "${MY_DIR}"
